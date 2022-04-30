73bff26919a36d1b1bca6c7051f14b6b09d28099
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-04-30 19:48:09 +0200

    Print every member of the guild

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const {Client, Intents} = require('discord.js');
+const dayjs = require('dayjs');
 
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
 });
 
+let currentDay = dayjs().minute();
+
+// Checking if it is a new day
+function checkNewDay(callback) {
+    if(currentDay < dayjs().minute() || (dayjs().minute() === 1 && currentDay !== 1)) {
+        currentDay = dayjs().minute();
+        callback();
+    }
+}
+
+function giveRole() {
+    const guild = client.guilds.resolve('802942121391816704');
+
+    guild.members.fetch().then(members => {
+        members.forEach(member => {
+            console.log(member.user.username);
+        });
+    });
+}
+
+async function checkTimeout() {
+    setTimeout(() => {
+        checkNewDay(giveRole);
+        checkTimeout();
+    }, 1000 * 1);
+}
+
 client.once('ready', () => {
-    console.log('Bot online!');
+    console.log('The BOT is online!');
+
+    checkTimeout();
 });
 
 client.login('ODE0MTcyMTMwOTQ1Nzk0MTI4.YDZ-4g.M03A5QLfZTNQ7prhWccjiqbgC_4');
