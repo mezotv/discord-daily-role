f3859b1d9f102aada98ffeab86f12ea3c89e54d2
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-05-01 16:48:26 +0200

    Role deletion and creation

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const client = new Client({
 });
 
 let currentDay = dayjs().minute();
+let roleID;
 
-// Checking if it is a new day
+// Calls 'callback' if it is a new day
 function checkNewDay(callback) {
     if(currentDay < dayjs().minute() || (dayjs().minute() === 1 && currentDay !== 1)) {
         currentDay = dayjs().minute();
@@ -22,22 +23,40 @@ function checkNewDay(callback) {
     }
 }
 
+// Gives the role specified by the slash command to a random user
 function giveRole() {
     const guild = client.guilds.resolve('802942121391816704');
 
     guild.members.fetch().then(members => {
-        let randomMember = members.random();
-        
-        console.log(randomMember);
+        const randomMember = members.random();
+        let role = guild.roles.cache.get(roleID);
 
-        // Custom nickname
-        randomMember.setNickname(
-            `[DSA] ${randomMember.user.username}`,
-            `Congratulazioni ${randomMember.user.username}, sei il DSA del giorno!`
-        );
+        /* ---- Deleting and recreating the role
+        (more efficient than removing the role) ---- */
+        role.delete();
+
+        role = guild.roles.create({
+            name: role.name,
+            color: role.color,
+            hoist: role.hoist,
+            position: role.position,
+            permissions: role.permissions,
+            mentionable: role.mentionable
+        });
+
+        roleID = role.id; // Updating the roleID
+
+        // // Updating the user
+        // randomMember.edit({
+        //     nick: `[DSA] ${randomMember.user.username}`,
+        //     roles: randomMember.roles.add(guild.roles.cache.some(role => {
+        //         role.id === '970329924051288184'
+        //     }))
+        // }, `Congratulazioni ${randomMember.user.username}, sei il DSA del giorno!`);
     });
 }
 
+// Asyncronous check, (called once per second)
 async function checkTimeout() {
     setTimeout(() => {
         checkNewDay(giveRole);
@@ -45,10 +64,39 @@ async function checkTimeout() {
     }, 1000);
 }
 
+// BOT started successfully
 client.once('ready', () => {
     console.log('The BOT is online!');
 
+    // Slash commands
+    let guild = client.guilds.cache.get('802942121391816704');
+    let commands = guild.commands;
+
+    commands?.create({
+        name: 'dailyrole',
+        description: 'Set the role that will be given each day.',
+        options: [
+            {
+                type: 'STRING',
+                name: 'roleid',
+                description: 'The ID of the role to set',
+                required: true
+            }
+        ]
+    })
+
     checkTimeout();
 });
+
+// Slash command interaction
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isCommand()) return;
+
+    const {commandName, options} = interaction;
+
+    if(commandName.toLowerCase() === 'dailyrole') {
+        roleID = options.data[0].value;
+    }
+})
 
 client.login(process.env.TOKEN);
